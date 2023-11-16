7d83681c35a4673c03ff64224b168fa2013e5d10
amevide998 <hdsvidi@gmail.com>
2023-11-16 17:17:00 +0700

    add api property for login user

--- src/dto/loginUser.dto.ts
+++ src/dto/loginUser.dto.ts
@@ -1,15 +1,25 @@
 import { IsEmail, IsNotEmpty, MaxLength } from "class-validator";
+import {ApiProperty} from "@nestjs/swagger";
 
 export class LoginUserDto {
     @IsNotEmpty()
     @IsEmail(undefined, {
         message: "Make sure to use a valid email address."
     })
+    @ApiProperty({
+        type: 'string',
+        format: 'email',
+
+    })
     email: String
 
     @IsNotEmpty()
     @MaxLength(40, {
         message: `password is too long, maximum length is $constraint1`
+    })
+    @ApiProperty({
+        type: 'string',
+        format: 'password',
     })
     password: String
 }
