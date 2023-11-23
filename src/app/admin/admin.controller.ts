351018f5d0dad18937cc383b53265cf5300e3d7c
amevide998 <hdsvidi@gmail.com>
2023-11-23 14:14:04 +0700

    add list endpoint

--- src/app/admin/admin.controller.ts
+++ src/app/admin/admin.controller.ts
@@ -1,9 +1,10 @@
-import {Body, Controller, HttpCode, HttpStatus, Post, Res, UsePipes, ValidationPipe} from "@nestjs/common";
+import {Body, Controller, HttpCode, HttpStatus, Post, Res, UseGuards, UsePipes, ValidationPipe} from "@nestjs/common";
 import {ApiBody, ApiTags} from "@nestjs/swagger";
 import {LoginAdminDto} from "../../dto/loginAdmin.dto";
 import { Response } from 'express';
 import {WebResponse} from "../../utils/webResponse";
 import {AdminService} from "./admin.service";
+import {AdminGuard} from "./admin.guard";
 
 
 @Controller("admin")
@@ -17,13 +18,27 @@ export class AdminController{
     @UsePipes(new ValidationPipe({transform: true}))
     @HttpCode(HttpStatus.OK)
     @ApiBody({
-        description: 'user login',
+        description: 'admin login',
         type: LoginAdminDto
     })
     async signin(@Body() request: LoginAdminDto, @Res() res: Response){
         const token = await this.adminService.login(request);
         return res.status(HttpStatus.OK)
             .json(new WebResponse(HttpStatus.OK, "success login", token, null))
+    }
+
+    @Post("list")
+    @UsePipes(new ValidationPipe({transform: true}))
+    @HttpCode(HttpStatus.OK)
+    @ApiBody({
+        description: 'starling list',
+        type: LoginAdminDto
+    })
+    @UseGuards(AdminGuard)
+    async List(@Res() res: Response , @Req() request: any){
+        const result = await this.adminService.getAll();
+        return res.status(HttpStatus.OK)
+            .json(new WebResponse(HttpStatus.OK, "success", result, null));
     }
 
 
