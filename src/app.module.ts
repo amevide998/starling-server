20d9353bf241f6704f2934ce1b803ebed87ce940
amevide998 <hdsvidi@gmail.com>
2023-11-23 13:49:11 +0700

    admin module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import {AuthModule} from "./app/auth/auth.module";
 import {StarlingModule} from "./app/starling/starling.module";
 import {UserModule} from "./app/user/user.module";
+import {AdminModule} from "./app/admin/admin.module";
 
 @Module({
-  imports: [AuthModule, StarlingModule, UserModule]
+  imports: [AuthModule, StarlingModule, UserModule, AdminModule]
 })
 export class AppModule {}
