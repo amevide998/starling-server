e444e1548a26105d117e3e662969d972be2039e8
amevide998 <hdsvidi@gmail.com>
2023-11-23 14:22:32 +0700

    clean up

--- src/app/starling/starling.service.ts
+++ src/app/starling/starling.service.ts
@@ -1,21 +1,9 @@
 import { BadRequestException, Inject, Injectable, NotFoundException, UnauthorizedException } from "@nestjs/common";
-import {Model, Schema} from "mongoose";
+import {Model} from "mongoose";
 import { User } from "../user/user.interface";
-import { CreateUserDto } from "../../dto/createUser.dto";
-import * as bcrypt from 'bcrypt';
-import { CreateUserResponseDto } from "../../dto/createUserResponse.dto";
-import sendMail from "../email-verification/sendmail";
-import { EmailVerification } from "../email-verification/emailVerification.interface";
-import { v4 } from "uuid";
-import { LoginUserDto } from "../../dto/loginUser.dto";
-import { JwtService } from "@nestjs/jwt";
 import {CreateStarlingDto} from "../../dto/createStarling.dto";
-import {Starling} from "../starling/starling.interface";
+import {Starling} from "./starling.interface";
 import {FirebaseStorageService} from "../../utils/firebaseStorage";
-import {OtpVerifyDto} from "../../dto/otpVerify.dto";
-import {Otp} from "../otp/otp.interface";
-import sendOtp from "../email-verification/sendOtp";
-import {AuthService} from "../auth/auth.service";
 
 @Injectable()
 export class StarlingService{
