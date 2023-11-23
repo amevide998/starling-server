0fa74a47a22b7d9b34958e3280b611847da1f8df
amevide998 <hdsvidi@gmail.com>
2023-11-23 13:50:01 +0700

    add admin module

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,22 @@
+import {jwtConstants} from "../auth/constant";
+import {JwtModule} from "@nestjs/jwt";
+import {DatabaseModule} from "../../database/database.module";
+import {Module} from "@nestjs/common";
+import {AdminService} from "./admin.service";
+import {AdminController} from "./admin.controller";
+
+@Module({
+    imports: [
+        DatabaseModule,
+        JwtModule.register({
+            global: true,
+            secret: jwtConstants.secret,
+            signOptions: {
+                expiresIn: '1 days'
+            }
+        }),
+    ],
+    controllers : [AdminController],
+    providers : [AdminService]
+})
+export class AdminModule{}
